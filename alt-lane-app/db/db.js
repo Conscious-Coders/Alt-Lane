329471e083bd7dfd7334779eabcdb40c613b5a5d
Mfstat27 <tracey_thomas@live.com>
2021-04-06 15:20:21 -0400

    corrected db name in db.js file

--- alt-lane-app/db/db.js
+++ alt-lane-app/db/db.js
@@ -5,7 +5,7 @@ const pg = require('pg-promise')()
 const db = pg({
   "host": process.env.DB_HOST,
   "port": 5432,
-  "database": "sleep_tracker",
+  "database": "alt_lane",
   "user": process.env.DB_USER,
   "password": process.env.DB_PASS
 })
