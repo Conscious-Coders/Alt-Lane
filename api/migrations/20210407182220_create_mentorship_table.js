393a40c568bcb13529efaab70f6eb797e84a0240
Code-33 <77262307+Code-33@users.noreply.github.com>
2021-04-07 15:14:03 -0400

    Create 20210407182220_create_mentorship_table.js

---
+++ api/migrations/20210407182220_create_mentorship_table.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('mentorship', function(table){
+        table.integer('mentor_id');
+        table.integer('mentee_id');
+
+        table.foreign('mentor_id').onDelete('CASCADE').references('id').inTable('mentors');
+        table.foreign('mentee_id').onDelete('CASCADE').references('id').inTable('mentees');
+
+        table.primary(['mentor_id', 'mentee_id']);
+    })
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTable('mentorship');
+};
