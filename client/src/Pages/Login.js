b5513c9383ebabc90637325c1db0493b928867ac
Shruti Tamrakar Tuladhar <35846599+ShrutiTamrakarTuladhar@users.noreply.github.com>
2021-04-08 14:56:48 -0400

    created the login page (#40)

---
+++ client/src/Pages/Login.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import Form from '../Components/Form'
+
+
+function Login () {
+  const { form, handleChange } = Form({
+    email: '',
+    password: ''
+  })
+  return (
+    <div>
+      <img src='/alt_lane_black.png' style={{ width: '125px', height: 'auto' }} alt="alt-line logo"/>
+      <div className='containter d-flex justify-content-center'>
+        <div className='card w-50'>
+          <div className='container card-body'>
+            <h3 className='card-title'>Login</h3>
+            <form>
+              <div className='mb-3 row'>
+                <label htmlFor='email' className='col-sm-2 col-form-label'>Email</label>
+                <div className='col-sm-10'>
+                  <input className='form-control' value={form.email} onChange={handleChange} type='email' id='email' name='email' />
+                </div>
+              </div>
+              <div className='mb-3 row'>
+                <label htmlFor='password' className='col-sm-2 col-form-label'>Password</label>
+                <div className='col-sm-10'>
+                  <input className='form-control' value={form.password} onChange={handleChange} type='password' id='password' name='password' />
+                </div>
+              </div>
+              <button href='#' className='btn btn-dark'>LOGIN</button>
+            </form>
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default Login
