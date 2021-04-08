a90221ab955c44fb3113b4e98ea52be93093f659
JolizbethBonilla <jolizbeth98@gmail.com>
2021-04-08 14:55:20 -0400

    (feat) add mentee nav

---
+++ client/src/Components/MenteeNavBar.js
@@ -0,0 +1,32 @@
+import { Link } from 'react-router-dom'
+
+function MenteeNavBar(){
+
+  return(
+      <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#"> 
+            <img src='/alt_lane_logo.png' style={{ width: '105px', height: 'auto' }} alt="" />
+            </a>
+            <div class=" d-flex" id="navbarNav">
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                  <a class="nav-link " aria-current="page" href="#">HOME</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">FIND MENTOR</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link " aria-current="page" href="#">PROFILE</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">SETTINGS</a>
+                </li>
+              </ul>
+            </div>    
+        </div>
+      </nav>
+
+  )
+}
+export default MenteeNavBar;
