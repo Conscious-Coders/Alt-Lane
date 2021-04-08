aa48e49c55308e33dfb9c3b3adfe9c9a10efa2b4
Jolizbeth Bonilla <jolizbeth@users.noreply.github.com>
2021-04-08 16:05:16 -0400

    added navbar and footer (#42)

---
+++ client/src/Components/MentorNavBar.js
@@ -0,0 +1,29 @@
+import { Link } from 'react-router-dom'
+
+function MentorNavBar(){
+
+  return(
+      <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#"> 
+            <img src='/alt_lane_logo.png' style={{ width: '105px', height: 'auto' }} alt="" />
+            </a>
+            <div className=" d-flex" id="navbarNav">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <a className="nav-link " aria-current="page" href="#">HOME</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link " aria-current="page" href="#">PROFILE</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">SETTINGS</a>
+                </li>
+              </ul>
+            </div>    
+        </div>
+      </nav>
+
+  )
+}
+export default MentorNavBar;
