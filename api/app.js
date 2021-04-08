28e7136f740c22eeb1e660764690e87d263bbb30
Code-33 <77262307+Code-33@users.noreply.github.com>
2021-04-07 22:35:37 -0400

    Crud api for users table (#23)
    * Delete scratch.js
    
    * Update app.js
    
    * Create db.js

--- api/app.js
+++ api/app.js
@@ -8,6 +8,9 @@ const cors = require('cors')
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
 const testAPIRouter = require('./routes/testAPI')
+
+const pg = require('pg-promise')
+const db = require('./db')
 const app = express()
 
 // view engine setup
@@ -25,6 +28,57 @@ app.use('/', indexRouter)
 app.use('/users', usersRouter)
 app.use('/testAPI', testAPIRouter)
 app.use('/test', testAPIRouter)
+
+app.get('/api/users', async function (request, response) {
+  try {
+    const data = await db.any('SELECT * FROM users')
+    return response.json({
+      data: data
+    })
+  } catch (err) {
+    response.status(404).send(err)
+  }
+})
+
+app.post('/api/users/add', async function (request, response) {
+  try {
+    await db.none('INSERT INTO users (first_name, last_name, email, password, photo_url) VALUES (${first_name}, ${last_name}, ${email}, ${password}, ${photo_url})', request.body)
+
+    return response.send(
+      `The following user has been added: ${request.body.first_name} ${request.body.last_name}`
+    )
+  } catch (err) {
+    response.status(404).send(err)
+  }
+})
+
+// update first name in users table
+app.patch('/api/users/update/:id', async function (request, response) {
+  try {
+    const updateUser = parseInt(request.params.id)
+    await db.any(`UPDATE users SET first_name=$<first_name> WHERE id=${updateUser}`, request.body)
+
+    return response.send(
+      `The following user id name has been updated: ${updateUser}`
+    )
+  } catch (err) {
+    response.status(404).send(err)
+  }
+})
+
+app.delete('/api/users/delete/:id', async function (request, response) {
+  try {
+    const deleteUser = parseInt(request.params.id)
+    await db.none('DELETE FROM users WHERE id=$1', deleteUser)
+
+    return response.send(
+      `The following user id has been deleted: ${deleteUser}`
+    )
+  } catch (err) {
+    response.status(404).send(err)
+  }
+})
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404))
