037490a62eb3a7a7e0ea1a1342a9c92e189ba031
Shruti Tamrakar Tuladhar <35846599+ShrutiTamrakarTuladhar@users.noreply.github.com>
2021-04-09 20:21:24 -0400

    (feat) added settings pages mvp (#52)

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,11 @@ import MenteeProfile from "./Pages/MenteeProfile"
 import MentorProfile from "./Pages/MentorProfile"
 import Landing from './Pages/Landing'
 import Homepage from './Pages/Homepage'
+<<<<<<< HEAD
+import Settings from './Pages/Settings'
+=======
 import FindMentor from './Pages/FindMentor'
+>>>>>>> 685c10d49373c16d6ff87f4890d2622138ceb916
 
 import './App.css'
 
@@ -43,7 +47,10 @@ function App () {
           <Route path='/profile/mentor'>
             <MentorProfile />
           </Route>
-          <Route path='/find-mentor'>
+          <Route path='/settings'>
+            <Settings isMentor={false}/>
+          </Route>
+         <Route path='/find-mentor'>
             <FindMentor />
           </Route>
         </Switch>
