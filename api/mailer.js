6c8edec987ebd60a313c453e17959b8d0253ac3d
Mfstat27 <tracey_thomas@live.com>
2021-04-18 14:54:10 -0400

    nodemailer stuff with some chat stuff

---
+++ api/mailer.js
@@ -0,0 +1,31 @@
+const nodeMailer = require('nodemailer')
+
+let mailer = nodeMailer.createTransport({
+    service:'gmail',
+    auth:{
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD
+    }
+})
+
+/**
+ * Send an email to a user
+ * 
+ * @param {Object} email 
+ */
+function sendEmail(email) {
+    mailer.sendMail(email, (err, info) => {
+        if(err) {
+            console.log('error in sending mail', err)
+            throw err;
+        }
+            
+        console.log('Email successfully sent!',info)
+    })
+}
+
+
+module.exports = {
+    mailer,
+    sendEmail
+};
