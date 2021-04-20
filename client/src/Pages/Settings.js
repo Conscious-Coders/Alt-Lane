4b24db3a58b7c4f19ae430de7c63934480ddca04
Shruti Tamrakar Tuladhar <35846599+ShrutiTamrakarTuladhar@users.noreply.github.com>
2021-04-19 23:30:16 -0400

    (fix) landing page - still in progress (#115)

--- client/src/Pages/Settings.js
+++ client/src/Pages/Settings.js
@@ -9,11 +9,9 @@ function Settings () {
 
   const { state: authState } = React.useContext(AuthContext);
   const userId = authState.user; 
-  const userType = authState.userType; 
   const authToken = authState.token; 
-
+  const [isDisable, setDisable] = useState(true)
   const [editBtn, setEditBtn]  = useState("Edit")
-
   const [userData, setUserData] = useState({
     email: '',
     oldPassword: '', 
@@ -88,8 +86,6 @@ function Settings () {
     return response; 
   }
  
-  const [isDisable, setDisable] = useState(true)
-
   function enableEdit () {
     if(isDisable === true) {
       setDisable(false)
@@ -103,69 +99,56 @@ function Settings () {
 
   const handleSubmit = async e =>  {
     e.preventDefault();
-
-    console.log(userData.email); 
-    console.log(userData.oldPassword); 
-    console.log(userData.newPassword); 
-
-    console.log("this is submiting")
-
     let isValidRequest = await checkPassword(); 
-
     if(isValidRequest.isVerified) {
       console.log("line 110 inside the if statemnt")
       updateUserInfo()
     } else {
       console.log("error! can't make changes")
     }
-
   }
 
   return(
     <div>
-        <LoginNav userType={userType} authToken={authToken}/>
-      <div className="container"  style={{ marginTop:"10%",  marginBottom:"10%"}}>
-     
-    <div className='containter d-flex justify-content-center'  style={{ marginTop:"1%"}}>
-    
-      <div className='card w-75 col-8' style={{ background:"linear-gradient(45deg, #A0AAE7 40%, #BA92F3 90%)"}}>
-        <div className='card-body'>
-          <div className="d-flex justify-content-end" style={{ marginBottom:"1%"}}>
-            <button className="btn btn-dark" onClick={enableEdit}>{editBtn}</button>
+      <LoginNav/>
+        <div className="container"  style={{ marginTop:"10%", height: "40vh", marginBottom:"10%"}}>
+          <div className='containter d-flex justify-content-center'  style={{ marginTop:"1%"}}>
+            <div className='card w-75 col-8' style={{ background:"linear-gradient(45deg, #A0AAE7 40%, #BA92F3 90%)"}}>
+              <div className='card-body'>
+                <div className="d-flex justify-content-end" style={{ marginBottom:"1%"}}>
+                  <button className="btn btn-dark" onClick={enableEdit}>{editBtn}</button>
+                </div>
+                <form onSubmit={handleSubmit}>
+                  <div className='mb-3 row'>
+                    <label htmlFor='firstName' className='col-sm-2 col-form-label'>Email</label>
+                    <div className='col-sm-10'>
+                      <input className='form-control' disabled={isDisable} value={userData.email} onChange={handleChange} type='email' id='email' name='email' />
+                    </div>
+                  </div>
+                  <div className='mb-3 row'>
+                    <label htmlFor='password' className='col-sm-2 col-form-label'>Old Password</label>
+                    <div className='col-sm-10'>
+                      <input className='form-control' disabled={isDisable} value={userData.oldPassword} onChange={handleChange} type='password' id='oldPassword' name='oldPassword' />
+                    </div>
+                  </div>
+                  <div className='mb-3 row'>
+                    <label htmlFor='password' className='col-sm-2 col-form-label'>New Password</label>
+                    <div className='col-sm-10'>
+                      <input className='form-control' disabled={isDisable} value={userData.newPassword} onChange={handleChange} type='password' id='newPassword' name='newPassword' />
+                    </div>
+                  </div>
+                  <div>
+                    <button type="submit" hidden={isDisable} className='btn btn-dark '>
+                        Submit
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
           </div>
-       
-          <form onSubmit={handleSubmit}>
-            <div className='mb-3 row'>
-              <label htmlFor='firstName' className='col-sm-2 col-form-label'>Email</label>
-              <div className='col-sm-10'>
-                <input className='form-control' disabled={isDisable} value={userData.email} onChange={handleChange} type='email' id='email' name='email' />
-              </div>
-            </div>
-            
-            <div className='mb-3 row'>
-              <label htmlFor='password' className='col-sm-2 col-form-label'>Old Password</label>
-              <div className='col-sm-10'>
-                <input className='form-control' disabled={isDisable} value={userData.oldPassword} onChange={handleChange} type='password' id='oldPassword' name='oldPassword' />
-              </div>
-            </div>
-            <div className='mb-3 row'>
-              <label htmlFor='password' className='col-sm-2 col-form-label'>New Password</label>
-              <div className='col-sm-10'>
-                <input className='form-control' disabled={isDisable} value={userData.newPassword} onChange={handleChange} type='password' id='newPassword' name='newPassword' />
-              </div>
-            </div>
-                <button href='#' className='btn btn-dark' >
-                     Submit
-                </button>
-          </form>
         </div>
-
-      </div>
-
+      <Footer/>
     </div>
-    </div>
-    <Footer/>
-  </div>
   )
 }
 export default Settings
