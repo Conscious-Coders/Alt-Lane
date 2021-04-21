7c77ddc60b7a85c316982cef6d78d7f1dc18019c
ShrutiTamrakarTuladhar <t.shruti98@gmail.com>
2021-04-21 14:48:00 -0400

    changes for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: api/bin/www.js
--- api/knexfile.js
+++ api/knexfile.js
@@ -29,15 +29,12 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password'
-    },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
     },
+    ssl: true, 
     migrations: {
       tableName: 'knex_migrations'
     }
