009890c97480b5b6a681276b1dea7e37f7d47d15
Jolizbeth Bonilla <jolizbeth@users.noreply.github.com>
2021-04-22 13:35:38 -0400

    Revert "(feat) add remove button to home card (#126)"
    This reverts commit 791990f24c3cfe9abc8e813919fa14a27ea2f2ba.

--- api/routes/mentorship.js
+++ api/routes/mentorship.js
@@ -185,6 +185,15 @@ router.patch('/', verifyToken, async function (request, response) {
 router.delete('/', verifyToken, async function (request, response) {
   const mentee = parseInt(request.body.mentee_id)
   const mentor = parseInt(request.body.mentor_id)
+
+    jwt.verify(request.token, 'secretKey', async (err, authData) => {
+      if(err){
+        response.sendStatus(403)
+      } 
+      else if(authData.data[0].user_id !== mentee){
+        response.sendStatus(500)
+        console.log('not working')
+      }else {
         try {
 
         await db.none(`DELETE FROM mentorship WHERE mentee_id=${mentee} AND mentor_id=${mentor}`)
@@ -193,7 +202,8 @@ router.delete('/', verifyToken, async function (request, response) {
         console.log(err)
         response.status(404).send(err)
       }
-
+    }
+  })
 })
 
 module.exports = router
