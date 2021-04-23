e0431d2f437f2e94927b32fb92b734fc946c702b
Mfstat27 <tracey_thomas@live.com>
2021-04-23 17:52:33 -0400

    trying again to fix no undefined check in verify token

--- api/middleware/verifytoken.js
+++ api/middleware/verifytoken.js
@@ -7,9 +7,8 @@ const verifyToken = async  (req, res, next) =>{
     }
     const bearer = authHeader.split(' ')
     const bearerToken = bearer[1]
-    if(bearerToken === undefined){
-        return next(new Error(`Sorry token is undefined so you do not have access to this page!`))
-    }
+    if(typeof bearerToken !== undefined){
+   
     try {
         const decodedToken = await jwt.verify(bearerToken, 
         process.env.RANDOM_TOKEN);
@@ -18,6 +17,7 @@ const verifyToken = async  (req, res, next) =>{
     } catch (err) {
         console.log(err)
        next(err)
-    }    
+    } 
+}   
 }
 module.exports = verifyToken
