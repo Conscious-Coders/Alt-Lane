865dd76cf4138971d40589450be2a8b81def8163
Jolizbeth Bonilla <jolizbeth@users.noreply.github.com>
2021-04-24 19:06:30 -0400

    add animations to chat (#174)

--- client/src/Components/Chat/Chat.js
+++ client/src/Components/Chat/Chat.js
@@ -29,24 +29,33 @@ const Chat = (props) => {
     return (
        <div>
           <LoginNav/>
-          <div className="outerContainer">
-          
-              <div className="innerChatContainer">
-                  <InfoBar room = {roomId}/>
-                  <Messages messages={messages} name={name} user={props.user}  />
-              <div className="formChat">
-                  <input
-                  className='inputChat text'
-                  value={newMessage}
-                  onChange={handleNewMessageChange}
-                  placeholder="Write message..."
-              />
-              <button onClick={handleSendMessage} className="sendButton" style={{background:"linear-gradient(345deg, #A0AAE7 40%, #BA92F3 90%)"}}>
-                  Send
-              </button>
-              </div>
-              </div>
-          
+          <div className="chatAnimPlane d-flex justify-content-around align-items-center">
+              <img src="../dm.gif" alt="plane left"/>
+              <h2>Chat Room</h2>
+              <img src="../dmRight.gif" alt="plane right"/>
+          </div>
+          <div className="outerContainer d-flex align-items-start ">
+            <div className="chatAnim d-flex  justify-content-start col-3 align-items-center">
+                <img src="../threePeople.gif" alt="people waving"/>
+            </div>
+            <div className="innerChatContainer col-6">
+                <InfoBar room = {roomId}/>
+                <Messages messages={messages} name={name} user={props.user}  />
+                <div className="formChat">
+                    <input
+                    className='inputChat text'
+                    value={newMessage}
+                    onChange={handleNewMessageChange}
+                    placeholder="Write message..."
+                    />
+                    <button onClick={handleSendMessage} className="sendButton" style={{background:"linear-gradient(345deg, #A0AAE7 40%, #BA92F3 90%)"}}>
+                        Send
+                    </button>
+                </div>
+            </div>
+            <div className="chatAnim d-flex justify-content-end col-3">
+              <img src="../mentoring.gif" alt="people waving"/>
+            </div>
           </div> 
           <Footer/>
        </div>
